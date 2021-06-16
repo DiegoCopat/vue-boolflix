cee2caab86633b712e3b78d16bf985d2baecd34b
DiegoCopat <diegocopat@gmail.com>
2021-06-16 16:55:42 +0200

    milestone 2.1

--- src/components/Element.vue
+++ src/components/Element.vue
@@ -2,7 +2,19 @@
     <div class="element">
         <h2 class="title">{{ movieElement.title }}</h2>
         <h2 class="originalTitle">{{ movieElement.original_title }}</h2>
-        <h2 class="language">{{ movieElement.original_language }}</h2>
+        
+        <img 
+            v-if="movieElement.original_language == 'en'" 
+            src="../assets/images/en.png" 
+            alt="">
+        <img 
+            v-else-if="movieElement.original_language == 'it'" 
+            src="../assets/images/it.png" 
+            alt="">
+        <h2 
+            class="language" 
+            v-else> {{ movieElement.original_language }} 
+        </h2>
         <h2 class="vote">{{ movieElement.vote_average }}</h2>
     </div>
 
@@ -21,8 +33,23 @@ export default {
 <style lang="scss" scope>
 
     .element {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        width: 19%;
+        height: 150px;
         margin: 5px;
         padding: 10px;
         border: 1px solid white;
+        font-size: 10px;
+
+        img {
+            width: 50px;
+        }
+
+        h2 {
+            width: 100%;
+        }
     }
 </style>
